c1f966cca606763493fca0f2bb48eb3824df7c73
Sakata-Eof <sakatae@nuaa.edu.cn>
2024-06-27 19:14:33 +0800

    添加随机数据生成器

---
+++ data_insertion.py
@@ -0,0 +1,26 @@
+import random
+from faker import Faker
+import database
+# 初始化Faker实例，并设置为中文
+fake = Faker('zh_CN')
+
+# 定义一些类别
+genres = ['科幻', '小说', '历史', '传记', '童话', '悬疑', '浪漫', '技术', '哲学', '诗歌']
+
+cur = database.DatabaseManager()
+
+
+for i in range(1000):
+    title = fake.sentence(nb_words=random.randint(1, 5))
+    author = fake.name()
+    genre = random.choice(genres)
+    year = random.randint(1, 2024)
+    cur.add_book(title, author, genre, year)
+
+    name = fake.name()
+    email = fake.company_email()
+    cur.add_borrower(name, email)
+
+    print(i)
+
+
